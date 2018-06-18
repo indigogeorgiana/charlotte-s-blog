0f672f7c1a71472e07864f8f54a19106af8f6446
Sam Sanders <sam-sanders@hotmail.co.uk>
2018-06-18 15:40:00 +1200

    css floats but not finished

--- client/components/footer.jsx
+++ client/components/footer.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Footer = props => {
   return (
-    <div>
-      <span>{props.copyright}</span>
-      <span>{props.author}</span>
+    <div className="clear">
+      <div className="footer">
+        <span>{props.copyright}</span>
+        <span>{props.author}</span>
+      </div>
     </div>
   )
 }
