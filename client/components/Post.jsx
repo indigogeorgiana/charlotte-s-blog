778d534592ddff4000c4861df70fde51e55b7e6c
ZeBin Xiao <juniorxiao01@gmail.com>
2018-06-18 16:39:25 +1200

    opacitated border bottom

--- client/components/Post.jsx
+++ client/components/Post.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 const Post = props => {
   return (
     <div className='post-subm'>
-      <div className='post-title'>
-        {props.title}
-      </div>
       <div className='post-date'>
         {props.date}
       </div>
-      <div className='post-comment'>
-        {props.commentCount}
+      <div className='post-title'>
+        {props.title}
       </div>
+      {/* <div className='post-comment'>
+        {props.commentCount}
+      </div> */}
       <div className='post-paragraph'>
         {props.paragraph}
       </div>
